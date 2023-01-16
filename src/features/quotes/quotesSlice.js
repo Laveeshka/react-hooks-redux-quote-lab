cea5846b22921230ebdf817cdb9ea7692ef63b0f
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-01-16 17:56:54 +1100

    complete lab with deprecated redux method

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,66 @@
 // Action Creators
 // TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: "quotes/add",
+    payload: quote
+  }
+}
+
+export const removeQuote = quoteId => {
+  return {
+    type: "quotes/remove",
+    payload: quoteId
+  }
+}
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: "quotes/upvote",
+    payload: quoteId
+  }
+}
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: "quotes/downvote",
+    payload: quoteId
+  }
+}
+
 
 // Reducer
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
-  return state;
+  switch (action.type){
+    case "quotes/add":
+      return [
+        ...state,
+        action.payload
+      ]
+
+    case "quotes/remove":
+      return state.filter(quote => quote.id !== action.payload);
+
+    case "quotes/upvote":
+      return state.map(quote => {
+        if(quote.id === action.payload){
+          quote.votes++;
+        }
+        return quote;
+      });
+
+    case "quotes/downvote":
+      return state.map(quote => {
+        if(quote.id === action.payload && quote.votes > 0){
+          quote.votes--;
+        }
+        return quote;
+      });
+
+      default:
+        return state;
+  }
+  //return state;
 }
